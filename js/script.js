39fefe3a5d64b1fd53f7c07e2579aa3cbceb62a8
Camilo Luna <camilo@camiloluna.com>
2018-03-23 18:21:12 -0500

    Include latest updates on Jekyll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+var SALE_ADDRESS = "0xAF702F4be0cAeCcc2a3B42ee5d7Dd725bFA20B65";
+var MULTISIG_ADDRESS = "0x44E7346C988EdB3b23e7098f500Ff40138fFA03a";
+var JSON_RPC_URL = "https://rinkeby.infura.io/bitnation";
+
+const ethersProvider = new ethers.providers.JsonRpcProvider(JSON_RPC_URL);
+
 $( document ).ready(function() {
+
+    //Calculate invested ETH
+    (function () {
+
+        var investedEth;
+
+        ethersProvider
+            .getBalance(SALE_ADDRESS)
+            .then(function (amount) {
+                investedEth = amount;
+                return ethersProvider.getBalance(MULTISIG_ADDRESS);
+            })
+            .then(function (amount) {
+                investedEth = investedEth.add(amount);
+                investedEth = investedEth.toString(10);
+                investedEth = new BigNumber(ethers.utils.formatUnits(investedEth, 'ether'));
+
+                $('#amount').text(investedEth.toFixed(3).toString());
+            })
+            .catch(console.error)
+
+    })();
+
     // Open mobile menu
     $(".top-nav-icon").click(function(){
         $( "#topnav" ).toggleClass( "visible" );
@@ -54,7 +83,7 @@ $( document ).ready(function() {
             }
         }]
     });
-    // Timeline slider 
+    // Timeline slider
     $('.timeline').slick({
         dots: false,
         infinite: false,
