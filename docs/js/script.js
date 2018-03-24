0504137637234720aeb1cdd55bf7701ed1bb46a5
Florian Lenz <flenz81@googlemail.com>
2018-03-24 15:01:54 +0100

    [eth] switched to external ms for eth calculation

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,32 +1,16 @@
-var SALE_ADDRESS = "0xAF702F4be0cAeCcc2a3B42ee5d7Dd725bFA20B65";
-var MULTISIG_ADDRESS = "0x44E7346C988EdB3b23e7098f500Ff40138fFA03a";
-var JSON_RPC_URL = "https://rinkeby.infura.io/bitnation";
-
-const ethersProvider = new ethers.providers.JsonRpcProvider(JSON_RPC_URL);
-
 $( document ).ready(function() {
 
-    //Calculate invested ETH
-    (function () {
-
-        var investedEth;
-
-        ethersProvider
-            .getBalance(SALE_ADDRESS)
-            .then(function (amount) {
-                investedEth = amount;
-                return ethersProvider.getBalance(MULTISIG_ADDRESS);
-            })
-            .then(function (amount) {
-                investedEth = investedEth.add(amount);
-                investedEth = investedEth.toString(10);
-                investedEth = new BigNumber(ethers.utils.formatUnits(investedEth, 'ether'));
-
-                $('#amount').text(investedEth.toFixed(3).toString());
-            })
-            .catch(console.error)
-
-    })();
+    $.ajax({
+        url: "https://ico-fund-calculation.herokuapp.com/",
+        success: function (data) {
+            if(typeof data.collected_eth === "number"){
+                $('#amount').text(data.collected_eth);
+                return;
+            }
+            console.log("collected_eth is not a number: "+JSON.stringify(data));
+        },
+        error: console.error
+    });
 
     // Open mobile menu
     $(".top-nav-icon").click(function(){
